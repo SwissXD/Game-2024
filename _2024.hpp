4efd6c71065f7b17881b5612972c157330e28f00
SwissXD <tobiaskeller1995@gmail.com>
2016-09-06 14:29:51 +0200

    Index problem in all class methods
    ::MoveDown
    ::MoveUp
    ::MoveRight

--- _2024.hpp
+++ _2024.hpp
@@ -67,7 +67,7 @@ class _2024Board
         }
 
     public:                 //0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
-        _2024Board(): Tokens{ 2, 0, 0, 0, 0, 4, 0, 0, 2, 0, 4, 0, 0, 4, 4, 0}
+        _2024Board(): Tokens{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8}
         {
 
         }
@@ -90,6 +90,62 @@ class _2024Board
             this->setFreePos(2);
         }
 
+        void MoveDown()
+        {
+             //------------//
+            // 0  1  2  3 //
+            // 4  5  6  7 //
+            // 8  9 10 11 //
+            //12 13 14 15 //
+            //------------//
+
+            //summUp Token
+            for(std::size_t i = 12; i < 16; ++i)
+            {
+                for(std::size_t j = i; j > i-7; j-=4)
+                {
+                    std::cout << "i: " << i << '\n' ;
+                    while(this->Tokens[j] == empty && j > 4)
+                    {
+                        j-=4;
+                    }
+
+                    unsigned j2 = j-4;
+                    /*if(j > 4)
+                    {
+                        j2 = j-4;
+                    }*/
+
+                    while(this->Tokens[j2] == empty && j2 > 4)
+                    {
+                        j2-=4;
+                    }
+                    std::cout << "j:   " << j << '\n';
+                    std::cout << "j2:  " << j2 << '\n';
+                    if(this->Tokens[j] == this->Tokens[j2])
+                    {
+                        this->Tokens[j] *= 2;
+                        this->Tokens[j2] = empty;
+                        if(j > 4)
+                        {
+                            j-=4;
+                        }
+                    }
+                }
+
+                for(std::size_t j = i; j > i-12; j-=4)
+                {
+                    unsigned counter = j;
+                    while(this->Tokens[j] == empty && counter > i-12)
+                    {
+                        this->Tokens[j] = this->Tokens[counter-4];
+                        this->Tokens[counter-4] = empty;
+                        counter-=4;
+                    }
+                }
+            }
+        }
+
         void MoveUp()
         {
             //------------//
@@ -104,7 +160,6 @@ class _2024Board
             {
                 for(std::size_t j = i; j < i+8; j+=4)
                 {
-
                     while(this->Tokens[j] == empty && j < 12)
                     {
                         j+=4;
@@ -135,6 +190,55 @@ class _2024Board
                         this->Tokens[j] = this->Tokens[counter+4];
                         this->Tokens[counter+4] = empty;
                         counter+=4;
+                    }
+                }
+            }
+        }
+
+        void MoveRight()
+        {
+            //------------//
+            // 0  1  2  3 //
+            // 4  5  6  7 //
+            // 8  9 10 11 //
+            //12 13 14 15 //
+            //------------//
+
+            //clear empty Token
+            for(std::size_t i = 3; i < 15; i+=4)
+            {
+                for(std::size_t j = i; j > i-3; --j)
+                {
+                    while(this->Tokens[j] == empty && j > i-3)
+                    {
+                        --j;
+                    }
+
+                    unsigned j2 = j-1;
+                    while(this->Tokens[j2] == empty && j2 > i-3)
+                    {
+                        --j2;
+                    }
+
+                    if(this->Tokens[j] == this->Tokens[j2])
+                    {
+                        this->Tokens[j] *= 2;
+                        this->Tokens[j2] = empty;
+                        if(j > i-4)
+                        {
+                            --j;
+                        }
+                    }
+                }
+
+                for(std::size_t j = i; j > i-3; --j)
+                {
+                    unsigned counter = j;
+                    while(this->Tokens[j] == empty && counter > i-3)
+                    {
+                        this->Tokens[j] = this->Tokens[counter-1];
+                        this->Tokens[counter-1] = empty;
+                        --counter;
                     }
                 }
             }
